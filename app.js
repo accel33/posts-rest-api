87195e510f2fd551eae1149429fec62247d1ab8e
Accel <accelwtf@gmail.com>
2019-07-15 22:02:19 +0700

    Implement route setup

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
+const feedRoutes = require("./routes/feed");
+
 const app = express();
+
+app.use("/feed", feedRoutes);
+
+// We could do routes in here using, app.use / app.post / app.put('/path') etc. But we will use the express router again
 
 app.listen(8080);
