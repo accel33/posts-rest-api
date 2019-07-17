bbaa263f144c7204adb72befad333348354b86c3
Accel <accelwtf@gmail.com>
2019-07-17 16:11:42 +0700

    Add feat, posts can be stored in database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   //! The most important is to pass error code to the client on APIs.
@@ -22,27 +23,30 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect",
-        errors: errors.array()
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array()
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    // todo... Success a resource was created
-    message: "Post created successfully",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: "Accel"
-      },
-      createdAt: new Date()
+  // * Declare db field
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/duca.jpg",
+    creator: {
+      name: "Accel"
     }
   });
+  post // * Send and store the data to mongodb
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result //! Result is the post we save on the db
+      });
+    })
+    .catch(err => console.log(err));
 };
