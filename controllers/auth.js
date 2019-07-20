2abf9214fbb7ffe8b0df5cd9197caa82b24f5e42
Accel <accelwtf@gmail.com>
2019-07-20 11:09:49 +0700

    Add login controller/route and Implement JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
@@ -27,6 +28,50 @@ exports.signup = (req, res, next) => {
     })
     .then(result => {
       res.status(201).json({ message: "User created.", userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  //! Get this info/data from the front-end
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  //! Search if there is a user with that email to login if true
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error("A user with this email could not be found.");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error("Wrong password");
+        error.statusCode = 401;
+        throw error;
+      }
+      //! Now we generate the JSON web Token
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+          //! The raw password is not stored here, because this would be return to the front-end, to the user to which the password belong. Even tho, it is not ideal
+        },
+        "somesupersecretsecret",
+        { expiresIn: "1h" }
+      );
+      //! Now we have a token that we can return to the client
+      //! Also in React we will be looking for that ID
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch(err => {
       if (!err.statusCode) {
